fdd37757e4fb67604c5ab75d99300ffb9a8378d7
riscen <riscen_@hotmail.com>
2018-06-25 23:41:08 -0500

    Code fights solutions: Interview practice

--- arrays/rotateImage.py
+++
@@ -1,8 +0,0 @@
-def rotateImage(a):
-    rows = len(a)
-    columns = len(a[0])
-    a_rotated = [[0 for r in range(rows)] for c in range(columns)]
-    for i in range(rows): 
-        for j in range(columns): 
-            a_rotated[j][columns-i-1] = a[i][j]
-    return a_rotated
